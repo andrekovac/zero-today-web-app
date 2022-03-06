e8b6a4300daf77641991cd34618c9591d9ee2eb0
André Kovac <me@andrekovac.com>
2022-03-06 18:58:46 +0100

    switch from sqlite to postgres

--- db/seeds.ts
+++ db/seeds.ts
@@ -1,4 +1,7 @@
-// import db from "./index"
+import { SecurePassword } from "blitz"
+import db from "./index"
+
+import { User } from "@prisma/client"
 
 /*
  * This seed function is executed when you run `blitz db seed`.
@@ -7,10 +10,35 @@
  * or https://github.com/Marak/Faker.js to easily generate
  * realistic data.
  */
+
+type UserTuple = [User["name"], User["email"], string, User["role"]]
+
+const seedUsers: UserTuple[] = [
+  ["André Kovac", "dev@andrekovac.com", "1234", "USER"],
+  ["admin", "admin@andrekovac.com", "1234", "ADMIN"],
+]
+
 const seed = async () => {
   // for (let i = 0; i < 5; i++) {
   //   await db.project.create({ data: { name: "Project " + i } })
   // }
+  await Promise.all(
+    seedUsers.map(async ([name, email, password, role]) => {
+      const hashedPassword = await SecurePassword.hash(password)
+      try {
+        await db.user.create({
+          data: {
+            name,
+            email,
+            hashedPassword,
+            role,
+          },
+        })
+      } catch (error) {
+        console.log("error while seeding:", error.message)
+      }
+    })
+  )
 }
 
 export default seed
